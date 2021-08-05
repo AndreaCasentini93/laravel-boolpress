c38f2cb72645f195f0f5ea0181dc2dd7e9fa2b8e
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-08-05 16:27:33 +0200

    Sending and Success Vue

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -4,11 +4,39 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
 
 class LeadController extends Controller
 {
+    // VALIDATION
+    private $mailValidationArray = [
+        'name' => 'required|max:60',
+        'email' => 'required|email|max:60',
+        'message' => 'required'
+    ];
+    private $mailValidationMessages = [
+        'name.required' => 'Inserisci nome!',
+        'name.max' => 'Il nome non può contenere più di 60 caratteri!',
+        'email.required' => 'Inserisci email!',
+        'email.email' => 'L\'email inserita non è valida!',
+        'email.max' => 'Il nome non può contenere più di 60 caratteri!',
+        'message.required' => 'Inserisci messaggio!'
+    ];
+
+    // SEND MAIL
     public function store(Request $request) {
         $data = $request->all();
+
+        $validator = Validator::make($data, $this->mailValidationArray, $this->mailValidationMessages);
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'errors' => $validator->errors()
+                ]
+            );
+        }
+
         return response()->json($data);
     }
 }
--- resources/js/pages/Contact.vue
+++ resources/js/pages/Contact.vue
@@ -3,20 +3,24 @@
         <div class="container">
             <h1 class="text-center mb-3">Contattaci</h1>
             <form @submit.prevent="sendMessage()">
+                <div v-if="success" class="text-center alert alert-success">Il messaggio è stato ricevuto!<br>Vi risponderemo appena possibile.</div>
                 <div class="form-group">
                     <label for="name">Nome</label>
-                    <input type="text" class="form-control" id="name" placeholder="Inserisci Email" v-model="name">
+                    <input type="text" class="form-control" id="name" placeholder="Inserisci Email" v-model="name" :class="errors.name? 'is-invalid':'' ">
+                    <small v-for="error, index in errors.name" :key="index">{{ error }}</small>
                 </div>
                 <div class="form-group">
                     <label for="email">Indirizzo Email</label>
-                    <input type="email" class="form-control" id="email" placeholder="Inserisci Email" v-model="email">
+                    <input type="email" class="form-control" id="email" placeholder="Inserisci Email" v-model="email" :class="errors.name? 'is-invalid':'' ">
+                    <small v-for="error, index in errors.email" :key="index">{{ error }}</small>
                 </div>
                 <div class="form-group">
                     <label for="message">Messaggio</label>
-                    <textarea class="form-control" id="message" rows="5" placeholder="Scrivi Messaggio" v-model="message"></textarea>
+                    <textarea class="form-control" id="message" rows="5" placeholder="Scrivi Messaggio" v-model="message" :class="errors.name? 'is-invalid':'' "></textarea>
+                    <small v-for="error, index in errors.message" :key="index">{{ error }}</small>
                 </div>
-                <div class="text-center">
-                    <button type="submit" class="btn btn-warning mt-3">Invia</button>
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="d-flex align-items-center btn btn-warning mt-3" :disabled="sending">{{ !sending? 'Invia':'' }}<i :class="sending? 'fas fa-spinner':'' "></i></button>
                 </div>
             </form>
         </div>
@@ -31,11 +35,14 @@ export default {
             name: '',
             email: '',
             message: '',
-
+            errors: {},
+            success: false,
+            sending: false
         }
     },
     methods: {
         sendMessage: function() {
+            this.sendign = true;
             axios
                 .post('http://127.0.0.1:8000/api/leads', {
                     name: this.name,
@@ -43,7 +50,18 @@ export default {
                     message: this.message
                 })
                 .then( res => {
-                    console.log(res.data);
+                    if (res.data.errors) {
+                        this.errors = res.data.errors;
+                        this.success = false;
+                        this.sendign = false;
+                    } else {
+                        this.errors = {};
+                        this.name = '';
+                        this.email = '';
+                        this.message = '';
+                        this.success = true;
+                        this.sendign = false;
+                    }
                 })
                 .catch( err => {
                     console.log(err);
@@ -72,6 +90,26 @@ export default {
                 font-size: 18px;
                 color: $base-color-0;
                 text-shadow: 1px 1px 2px $black;
+            }
+
+            small {
+                color: $special-red;
+                text-shadow: 1px 1px 2px $black;
+                letter-spacing: 1px;
+            }
+
+            i {
+                font-size: 20px;
+                color: $black;
+                animation: rotation 1s infinite linear;
+            }
+            @keyframes rotation {
+                from {
+                    transform: rotate(0deg);
+                }
+                to {
+                    transform: rotate(360deg);
+                }
             }
 
         }
