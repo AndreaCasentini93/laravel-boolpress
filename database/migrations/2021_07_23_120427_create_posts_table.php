87077bfa96dcec43dc0f613662fe48af9f8f276e
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-08-05 14:31:54 +0200

    Database Refactoring

--- database/migrations/2021_07_23_120427_create_posts_table.php
+++ database/migrations/2021_07_23_120427_create_posts_table.php
@@ -15,9 +15,12 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('SET NULL');
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('content');
+            $table->string('cover')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2021_08_05_113459_create_leads_table.php
+++ database/migrations/2021_08_05_113459_create_leads_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddCoverPostsTable extends Migration
+class CreateLeadsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,12 @@ class AddCoverPostsTable extends Migration
      */
     public function up()
     {
-        Schema::table('posts', function (Blueprint $table) {
-            $table->string('cover')->nullable()->after('content');
+        Schema::create('leads', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60);
+            $table->string('email', 60);
+            $table->text('message');
+            $table->timestamps();
         });
     }
 
@@ -25,8 +29,6 @@ class AddCoverPostsTable extends Migration
      */
     public function down()
     {
-        Schema::table('posts', function (Blueprint $table) {
-            $table->dropColumn('cover');
-        });
+        Schema::dropIfExists('leads');
     }
 }
